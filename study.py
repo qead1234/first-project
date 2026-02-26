302a65f4c7f87db091a5ed7bb29d07f989266cbc
qead1234 <qpal030430@gmail.com>
2026-02-27 05:14:09 +0900

    조건문 활용 참/거짓

--- study.py
+++ study.py
@@ -4,12 +4,12 @@ height = float(input("키를 입력하세요: "))   # 실수 (Float)
 man = True # 불리언 (Boolean - 참/거짓)
 adult = True
 sex = str(input("성별을 입력하세요: "))
-if sex == "여자": #~라면 실행 (if))
+if sex == "여자": #~라면 실행 (if)
     man = False
 
 if age < 20:
     adult = False
-    
+
 print(f"{name}의 나이는 {age}살이고, 키는 {height}cm야.")
 print(f"남자인가요? {man}")
 print(f"성인인가요? {adult}")
